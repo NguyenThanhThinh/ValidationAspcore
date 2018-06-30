6d45a2dcfe94a902992f6bcd1bf0b1e968ad7aef
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-07-01 05:45:08 +0700

    fix message

--- ValidationAspNetCore/ValidationAspNetCore/Models/Employee.cs
+++ ValidationAspNetCore/ValidationAspNetCore/Models/Employee.cs
@@ -5,12 +5,12 @@ namespace ValidationAspNetCore.Models
     public class Employee
     {
         [Display(Name ="Họ và tên")]
-        [MaxLength(10,ErrorMessage ="Tên phải lớn hơn 10 ký tự")]
+        [MinLength(10,ErrorMessage ="Tên phải lớn hơn 10 ký tự")]
         public string FullName { get; set; }
 
         [Display(Name = "Tuổi")]
-        [Required(ErrorMessage = "Không để trống!")]
+        [Required(ErrorMessage = "Tuổi bắt buộc nhập")]
         [Range(18, 60, ErrorMessage = "Tuổi phải từ 18 đến 60 !")]
-        public int Age { get; set; }
+        public int? Age { get; set; }
     }
 }
