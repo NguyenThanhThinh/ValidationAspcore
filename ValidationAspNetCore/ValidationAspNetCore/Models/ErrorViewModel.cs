ca642d61fa456266543f60f777b44a70488a2967
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-07-01 05:14:44 +0700

    init

---
+++ ValidationAspNetCore/ValidationAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ValidationAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
