521d4c5e7bee6ba226adcd5d1731957ee1a49c29
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-07-01 05:34:07 +0700

    add view

---
+++ ValidationAspNetCore/ValidationAspNetCore/Controllers/DemoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValidationAspNetCore.Controllers
+{
+    using ValidationAspNetCore.Models;
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Validate(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "PASS");
+            }
+
+            return View(nameof(Index));
+        }
+    }
+}
